1cdf90789637b13efbc5d84aad39267f7e05a712
Jun Guo <gjuoun@gmail.com>
2022-02-21 23:35:27 -0400

    now it compiles tsx

---
+++ swc-react-tsx/dist/index.js
@@ -0,0 +1,6 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+import React from "react";
+import reactDom from "react-dom";
+reactDom.render(/*#__PURE__*/ _jsx("div", {
+    children: "test"
+}), document.querySelector("#root"));
---
+++ swc-react-tsx/src/index.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import reactDom from "react-dom";
+
+reactDom.render(<div>test</div>, document.querySelector("#root"));
